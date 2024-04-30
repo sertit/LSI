83e6da5d1257c2aa0998f0e485ae7fbf5df3db1c
Oscar NARVAEZ <onarvaez@SDS.local>
2024-04-30 11:48:58 +0200

    added lsi calculation
    initial structure for calculating LSI with 7 steps

--- lsi/lsi_core.py
+++ lsi/lsi_core.py
@@ -3,12 +3,30 @@ import logging
 from enum import unique
 
 from sertit.misc import ListEnum
+from sertit import AnyPath
+from sertit.unistra import get_geodatastore, s3_env
 
 # from lsi import LOGGER_NAME # on the meantime to solve the acces to lsi.py
 
 DEBUG = False
 LOGGING_FORMAT = "%(asctime)s - [%(levelname)s] - %(message)s"
 LOGGER = logging.getLogger("LSI")  # on the meantime to solve the acces to lsi.py
+
+
+def geodatastore(ftep=False):
+    """
+    This function returns the root path to the geo data store (DEM, Soil database...).
+    Args:
+        ftep: If True, the path to the s3 bucket for the ftep platform is returned. Else, get_geodatastore from sertit utils module is called.
+
+    Returns:
+        The path to the geo data store.
+
+    """
+    if ftep:
+        return AnyPath("s3://eo4sdg-data") #TODO
+    else:
+        return get_geodatastore()
 
 
 class DataPath:
@@ -41,4 +59,26 @@ def lsi_core() -> str:
     """
     TODO: Complete arguments and dosctring
     """
+
+    # 1. GEOLOGY
+
+    # 2. SLOPE (DEGREES)
+
+    # 3. LANDCOVER
+
+    # 4. ELEVATION
+
+    # 5. DISTANCE TO HYDRO
+
+    # 6. ASPECT
+
+    # 7. FINAL WEIGTHS
+
+    # 8. LSI RASTER CALCULATION
+
+
+
+
+
+
     raise NotImplementedError

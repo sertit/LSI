0e48125543ec4e007b16b4525adc64c2f5740d9c
NARVAEZ-LUCES OSCAR-DAVID-RAFAEL <narvaezluces@unistra.fr>
2024-06-03 06:03:41 +0000

    Delete launch_lsi.sh

--- lsi/launch_lsi.sh
+++
@@ -1,8 +0,0 @@
-python lsi_cli.py
--aoi = D:\\Oscar_Narvaez\\02_Projects\\02_EO4SDG\\01_Data\\S2_tiles_BRA_AOI.shp
--dem =  \\ds2\\database02\\BASES_DE_DONNEES\\GLOBAL\\COPDEM_30m
--lc = \\ds2\\database02\\BASES_DE_DONNEES\\GLOBAL\\ESA_WorldCover\\2021
--glim = \\ds2\\database03\\RMS\\Guide_CEMS_RRM\\Guide_STD\\5_Tools\\P17_LSI_data_tools_GMV\\Outside\\LiMW_GIS 2015\\LiMW_GIS_2015.gdb
--dbf = \\ds2\\database03\\RMS\\Guide_CEMS_RRM\\Guide_STD\\5_Tools\\P17_LSI_data_tools_GMV\\Outside\\DBF\\Weights_Outside\\Geology.dbf
--field = "Rating"
--out = D:\\Oscar_Narvaez\\02_Projects\\02_EO4SDG\\03_Output\\
